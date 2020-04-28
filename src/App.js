fb6cb99dfd40b85c6f613f6f2911eece7105f36f
Ross Gartland <rgartland117@hotmail.com>
2020-04-28 20:04:14 +0100

    Nav created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import MyNav from './components/nav/nav.component';
 import Home from './pages/home/home.component';
 import Portfolio from './pages/portfolio/portfolio.component';
 import './App.css';
@@ -6,6 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <MyNav/>
       <Home/>
       <Portfolio/>
     </div>
