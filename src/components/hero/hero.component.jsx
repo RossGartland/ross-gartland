46928b680ba3ec9bfe87ef9b4a3aaf9444d516b0
Ross Gartland <rgartland117@hotmail.com>
2020-04-28 17:40:36 +0100

    Hero created

---
+++ src/components/hero/hero.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './hero.style.css';
+
+const Hero = () => {
+    return (
+        <div className="hero-text">
+          <h1>ROSS GARTLAND</h1>
+          <hr/>
+          <p>I AM A SOFTWARE DEVELOPER</p>
+          <a href="#/">VIEW MY PORTFOLIO</a>
+        </div>
+    )
+}
+export default Hero;
---
+++ src/pages/home/home.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from  '../../components/hero/hero.component';
+import './home.style.css';
+
+const Home = () => {
+    return (
+        <div>
+            <Hero/>
+        </div>
+    )
+}
+export default Home;
