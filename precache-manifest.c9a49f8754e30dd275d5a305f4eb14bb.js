b7914d7fd9753e66ba192a2550fd67119bc93d50
Ross Gartland <rgartland117@hotmail.com>
2020-04-29 15:56:03 +0100

    Updates

---
+++ precache-manifest.c9a49f8754e30dd275d5a305f4eb14bb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a65cfd6463abe491936ad8560f5ac1ab",
+    "url": "/ross-gartland/index.html"
+  },
+  {
+    "revision": "3d7d3ab2fe50967889f8",
+    "url": "/ross-gartland/static/css/main.85b3a407.chunk.css"
+  },
+  {
+    "revision": "48a3d49d23f3a21c821b",
+    "url": "/ross-gartland/static/js/2.cab84578.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ross-gartland/static/js/2.cab84578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d7d3ab2fe50967889f8",
+    "url": "/ross-gartland/static/js/main.fc8784d0.chunk.js"
+  },
+  {
+    "revision": "78d7ed2576c3cdd13db2",
+    "url": "/ross-gartland/static/js/runtime-main.16fea994.js"
+  },
+  {
+    "revision": "7143a49f2107ad0eb01aaca79a9fff12",
+    "url": "/ross-gartland/static/media/code.7143a49f.jpg"
+  },
+  {
+    "revision": "6b88da1a437e38e0d12a4f6ca40c26ed",
+    "url": "/ross-gartland/static/media/covid19-ireland.6b88da1a.png"
+  },
+  {
+    "revision": "2b6e3651513c61a8a2441d6776d5833b",
+    "url": "/ross-gartland/static/media/self.2b6e3651.jpg"
+  },
+  {
+    "revision": "6a9d678d39afb26b74bfaf58fb169c10",
+    "url": "/ross-gartland/static/media/temp.6a9d678d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.adb16e5c7b70aeb675191107d74aa1d7.js"
+  "/ross-gartland/precache-manifest.c9a49f8754e30dd275d5a305f4eb14bb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ross-gartland/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
